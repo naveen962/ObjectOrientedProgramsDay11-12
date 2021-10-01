ed044da54e9dd9b96ee1dbf8acd9fb09c7ee7246
naveen962 <mrn28897@gmail.com>
2021-10-01 03:48:33 -0700

    InventoryManagement program is added

---
+++ InventoryManagementProgram/InventoryManagementProgram/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryDataManagement
+{
+    public class Inventory
+    {
+        public List<TypesOfRice> TypesOfRice;
+        public List<TypesOfPulses> TypesOfPulses;
+        public List<TypesOfWheat> TypesOfWheat;
+    }
+
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int Price;
+    }
+
+    public class TypesOfPulses
+    {
+        public string name;
+        public int weight;
+        public int Price;
+    }
+    public class TypesOfWheat
+    {
+        public string name;
+        public int weight;
+        public int Price;
+    }
+}
